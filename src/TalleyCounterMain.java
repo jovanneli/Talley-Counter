2985997cff39cd5905649b494c336b74d9cc6ff4
jli19 <jli19@HS-MATH2-01.gvsd.org>
2018-09-26 13:22:08 -0400

    Update TalleyCounter

--- src/TalleyCounterMain.java
+++ src/TalleyCounterMain.java
@@ -8,6 +8,7 @@ public class TalleyCounterMain
 	public static void main(String[] args)
 	{
 		TalleyCounter papaoutai = new TalleyCounter();
+		System.out.println(papaoutai.getCount());
 		papaoutai.click();
 		papaoutai.click();
 		papaoutai.click();
@@ -17,5 +18,11 @@ public class TalleyCounterMain
 		papaoutai.reset();
 		System.out.println(papaoutai.getCount());
 	
+		TalleyCounter daddyouthicc = new TalleyCounter(69);
+		System.out.println(daddyouthicc.getCount());
+		daddyouthicc.click();
+		daddyouthicc.click();
+		System.out.println(daddyouthicc.getCount());
+		
 	}
 }
